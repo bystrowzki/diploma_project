3ea79a4ee869b98cdfe7dc97506634f04778eddf
Alexandra Bystrovskaya <bystrowzki@inbox.ru>
2021-07-16 21:28:05 +0600

    Fixed

--- project.py
+++ project.py
@@ -1,31 +1,17 @@
 import requests
 import json
+import datetime
 from pprint import pprint
 from tqdm import tqdm
 from time import sleep
 
+
 with open('text.txt', 'r') as f:
     token_vk = f.read().strip()
 
-user_id = int(input('Ведите ID пользователя VK: '))
-token_ya = int(input('Ведите токен пользователя Яндекс.Диск: '))
-
-
-def get_photos():
-    url = 'https://api.vk.com/method/photos.get'
-    params = {
-        'owner_id': user_id,
-        'album_id': 'profile',
-        'extended': '1',
-        'count': 10,
-        'access_token': token_vk,
-        'v': '5.131'
-    }
-    res = requests.get(url, params=params, timeout=5)
-    return res.json()
-
 
 def personal_info():
+    info_list = []
     url = 'https://api.vk.com/method/users.get'
     params = {
         'user_ids': user_id,
@@ -33,8 +19,23 @@ def personal_info():
         'v': '5.131'
     }
     res = requests.get(url, params=params, timeout=5)
-    info = res.json()['response'][0]['first_name'] + ' ' + res.json()['response'][0]['last_name']
-    return info
+    info_list.append(res.json()['response'][0]['first_name'] + ' ' + res.json()['response'][0]['last_name'])
+    info_list.append(res.json()['response'][0]['id'])
+    return info_list
+
+
+def get_photos():
+    url = 'https://api.vk.com/method/photos.get'
+    params = {
+        'owner_id': personal_info()[1],
+        'album_id': 'profile',
+        'extended': '1',
+        'count': amount,
+        'access_token': token_vk,
+        'v': '5.131'
+    }
+    res = requests.get(url, params=params, timeout=5)
+    return res.json()
 
 
 def get_info():
@@ -43,19 +44,21 @@ def get_info():
     data = get_photos()
     x = -1
     for userpic in range(len(data['response']['items'])):
-        if data['response']['items'][userpic]['likes']['count'] in photo_data:
-            photo_data[data['response']['items'][userpic]['date']] = \
-                data['response']['items'][userpic]['sizes'][-1]['url']
+        date = str((datetime.datetime.fromtimestamp(int(data['response']['items']
+                                                        [userpic]['date']))).strftime('%d%b%Y'))
+        likes_amount = str(data['response']['items'][userpic]['likes']['count'])
+        url = data['response']['items'][userpic]['sizes'][-1]['url']
+        if likes_amount in photo_data:
+            photo_data[likes_amount+'_'+date] = url
         else:
-            photo_data[data['response']['items'][userpic]['likes']['count']] = \
-                data['response']['items'][userpic]['sizes'][-1]['url']
+            photo_data[likes_amount] = url
     for name in photo_data:
         data_dict = {}
         x += 1
         data_dict['file_name'] = str(name)+'.jpeg'
         data_dict['size'] = data['response']['items'][x]['sizes'][-1]['type']
         data_list.append(data_dict)
-    with open(f'{personal_info()}.json', 'w') as w_file:
+    with open(f'{personal_info()[0]}.json', 'w') as w_file:
         json.dump(data_list, w_file)
     return photo_data
 
@@ -75,7 +78,7 @@ def get_files_list():
 
 
 def new_folder():
-    folder_name = personal_info()
+    folder_name = personal_info()[0]
     upload_url = 'https://cloud-api.yandex.net/v1/disk/resources'
     headers = get_headers()
     params = {'path': folder_name}
@@ -97,8 +100,12 @@ def upload_photo_by_url():
             sleep(0.1)
             bar.update(1)
         bar.close()
-    with open(f'{personal_info()}.json', 'r') as r_file:
+    with open(f'{personal_info()[0]}.json', 'r') as r_file:
         pprint(json.load(r_file))
 
 
-upload_photo_by_url()
+if __name__ == "__main__":
+    user_id = input('Ведите ID или username пользователя VK: ')
+    amount = int(input('Введите количество сохраняемых фото: '))
+    token_ya = input('Ведите токен пользователя Яндекс.Диск: ')
+    upload_photo_by_url()
